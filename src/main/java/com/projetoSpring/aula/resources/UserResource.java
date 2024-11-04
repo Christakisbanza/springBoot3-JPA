20c5e20c8060fb8f6a9d2904e435ad1e21951356
Christakis <banzachristakis@gmail.com>
2024-11-04 17:25:57 -0300

    User entity and resource

---
+++ src/main/java/com/projetoSpring/aula/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.projetoSpring.aula.resources;
+
+
+import com.projetoSpring.aula.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @GetMapping
+    public ResponseEntity<User> findAll(){
+        User user  = new User(1L,"Itachi","Itachi@gmail.com", "41999","1234567");
+        return ResponseEntity.ok().body(user);
+    }
+}
